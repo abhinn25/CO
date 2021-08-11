c9868219490ba74668dcbc5425ac8f1741b31970
ut-god <88653523+ut-god@users.noreply.github.com>
2021-08-11 22:08:33 +0530

    Update COproject.py

--- CO_M21_Assignment-main/Simple-Assembler/COproject.py
+++ CO_M21_Assignment-main/Simple-Assembler/COproject.py
@@ -29,14 +29,14 @@ registers = {"R0": ["000", -10],
              }
 
 register_wo_flag = {"R0": ["000", -10],
-             "R1": ["001", -10],
-             "R2": ["010", -10],
-             "R3": ["011", -10],
-             "R4": ["100", -10],
-             "R5": ["101", -10],
-             "R6": ["110", -10],
-             "FLAGS": ["111", [0, 0, 0, 0]]  # V / L / G / E
-             }
+                    "R1": ["001", -10],
+                    "R2": ["010", -10],
+                    "R3": ["011", -10],
+                    "R4": ["100", -10],
+                    "R5": ["101", -10],
+                    "R6": ["110", -10],
+                    "FLAGS": ["111", [0, 0, 0, 0]]  # V / L / G / E
+                    }
 
 f2 = open("output.txt", "w+")
 
@@ -93,71 +93,72 @@ def Xor(reg1, reg2, reg3):
 def Invert(reg1, reg2):
     registers[reg1][1] = ~registers[reg2][2]
 
-def errorA(lisa,lno):
+
+def errorA(lisa, lno):
     if len(lisa) == 4:
         if lisa[1] in register_wo_flag.keys() and lisa[2] in register_wo_flag.keys() and lisa[3] in register_wo_flag.keys():
             return True
         else:
-            print("register name error",lno)
+            print("register name error", lno)
             return False
     else:
-        print("wrong syntax error",lno)
+        print("wrong syntax error", lno)
         return False
 
 
-def errorB(lisb,lno):
+def errorB(lisb, lno):
     if len(lisb) == 3:
         if lisb[1] in register_wo_flag.keys() and lisb[2][0] == "$" and 0 <= lisb[2][1:] <= 255:
-
             return True
-        else:
-
+        elif lisb[2][1:]<0 or lisb[2][1:]>255:
+            print("IMMEDIATE out of range",lno)
             return False
+        else:
+            print("register name error", lno)
+            return False
+
     else:
-        print("wrong syntax error",lno)
+        print("wrong syntax error", lno)
         return False
 
 
-def errorC(lisa,lno):
+def errorC(lisa, lno):
     if len(lisa) == 3:
         if lisa[1] in register_wo_flag.keys() and lisa[2] in register_wo_flag.keys():
             return True
         else:
-            print("register name error",lno)
+            print("register name error", lno)
             return False
     else:
-        print("wrong syntax error",lno)
+        print("wrong syntax error", lno)
         return False
 
 
-def errorD(lisd,lno):
-    if len(lisd) == 2:
-        if lisd[1] in var_dic.keys():
+def errorD(lisd, lno):
+    if len(lisd) == 3:
+        if lisd[1] in register_wo_flag.keys() and lisd[2] in var_dic.keys():
             return True
+        elif lisd[1] not in register_wo_flag.keys():
+             print("reggister error",lno)
+             return False
         else:
+            print("use of undefinded variables ",lno)
             return False
     else:
-        print("wrong syntax error",lno)
+        print("wrong syntax error", lno)
         return False
 
 
-def errorE(lise,lno):
+def errorE(lise, lno):
     if len(lise) == 2:
         if lise[1] in label_dic:
             return True
         else:
+            print("Use of undefined label ", lno)
             return False
     else:
-        print("wrong syntax error",lno)
+        print("wrong syntax error", lno)
         return False
-
-
-
-
-
-
-
-
 
 
 def error(lis):
@@ -167,112 +168,119 @@ def error(lis):
         if ins[0] == "mov":
             if len(ins) == 3:
                 if ins[1] in register_wo_flag.keys() and (ins[2] in register_wo_flag.keys() or (ins[2][0] == "$" and 0 <= int(ins[2][1:]) <= 255)):
-                    continue
+                   continue # have to deal intermeditae error here
+
                 else:
-                    print("register name error",i)
+                    print("register name error", i)
+
                     return False
-                    flag = 1
-                    break
+
             else:
-                print("wrong syntax error",i)
+                print("wrong syntax error", i)
+
                 return False
-                break
+
 
         if ins[0] == "var":
-            if len(ins) == 2:
+            if len(ins) == 3:
                 continue
             else:
-                break
+                print("gg1")
 
         if ins[0][-1] == ":":
             if ins[1] in OPcode.keys():
                 if OPcode[ins[1]][1] == "A":
-                    if errorA(ins,i)==False:
+
+                    if errorA(ins, i) == False:
                         return False
                 elif OPcode[ins[1]][1] == "B":
-                    if errorB(ins,i)==False:
+                    if errorB(ins, i) == False:
                         return False
                 elif OPcode[ins[1]][1] == "C":
-                    if errorC(ins,i)==False:
+                    if errorC(ins, i) == False:
                         return False
                 elif OPcode[ins[1]][1] == "D":
-                    if errorD(ins,i)==False:
+                    if errorD(ins, i) == False:
                         return False
                 elif OPcode[ins[1]][1] == "E":
-                    if errorE(ins,i)==False:
+                    if errorE(ins, i) == False:
                         return False
-               # elif OPcode[ins[1]][1] == "F":
-                  #  errorF(ins)
+            # elif OPcode[ins[1]][1] == "F":
+            #  errorF(ins)
             else:
                 print("type a typo in inst")
                 return False
-                
+
         elif ins[0] in OPcode.keys():
             if OPcode[ins[0]][1] == "A":
-                if errorA(ins,i)==False:
+                if errorA(ins, i) == False:
                     return False
             elif OPcode[ins[0]][1] == "B":
-                if errorB(ins,i)==False:
+                if errorB(ins, i) == False:
                     return False
             elif OPcode[ins[0]][1] == "C":
-                if errorC(ins,i)== False:
+                if errorC(ins, i) == False:
                     return False
             elif OPcode[ins[0]][1] == "D":
-                if errorD(ins,i) == False:
+                if errorD(ins, i) == False:
                     return False
             elif OPcode[ins[0]][1] == "E":
-                if errorE(ins,i)== False:
+                if errorE(ins, i) == False:
                     return False
-           # elif OPcode[ins[0]][1] == "F":
-             #   errorF(ins)
+        # elif OPcode[ins[0]][1] == "F":
+        #   errorF(ins)
         else:
-            print("type a typo in inst",i)
-            break
+            print("type a typo in inst", i)
+            return False
     return True
+
 
 f = open("input.txt", "r")
 lines = [line.rstrip().split() for line in f]
 
 final_input = []
-
 label_list = []  # stores label with its memory address. example - ["label:" ,"11"]
-label_dic={}
+label_dic = {}
 var_dic = {}
 var_list = []
 temp_var_list = []  # stores variable instructions
 program_counter = 0
 
-if error(lines)==True:  # error code
 
-    for inst in lines:
-        if inst != []:
-
+for inst in lines:
+    if inst != []:
             if (inst[0] == "var"):
                 temp_var_list.append(inst)
                 continue
             if (inst[0][-1] == ':'):
-                label_dic[inst[0]]= program_counter
+                label_dic[inst[0]] = program_counter
                 label_list.append([inst[0], program_counter])
                 final_input.append(inst)
                 program_counter += 1
             else:
                 final_input.append(inst)
                 program_counter += 1
-    final_input.extend(temp_var_list)
 
-    for i in (range(len(final_input))):
+
+final_input.extend(temp_var_list)
+
+
+
+for i in (range(len(final_input))):
         if final_input[i][0] == "var":
             final_input[i].append(i)
-            var_dic[final_input[i][1]]=i
+            var_dic[final_input[i][1]] = i
             var_list.append(final_input[i])
 
-    print(var_dic)
-    print(label_dic)
-    print(lines)
-    print(var_list)
-    print(label_list)
-    print(final_input)
 
+print(var_dic)
+print(label_dic)
+
+#print(var_list)
+#print(label_list)
+#print(final_input)
+
+if error(lines) == True:
     for inst in final_input:
 
         if inst[0] == "add":
