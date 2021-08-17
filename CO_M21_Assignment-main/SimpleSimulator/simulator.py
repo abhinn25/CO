ceb1922f80a90e930ce063a2846b4ea6e33154c0
abhinn25 <88654431+abhinn25@users.noreply.github.com>
2021-08-17 21:10:46 +0530

    Update simulator.py

--- CO_M21_Assignment-main/SimpleSimulator/simulator.py
+++ CO_M21_Assignment-main/SimpleSimulator/simulator.py
@@ -41,79 +41,91 @@ for i in range(len(lines)):
 #print(registers)
 #print(mem , len(mem))
 
+
 def binaryToDecimal(n):
     return int(n,2)
 
+
 def a_type(inst):
 
-    if OPcode[instruction[0:5]][0] == "add":
+    if OPcode[inst[0:5]][0] == "add":
         registers[inst[7:10]][1] = registers[inst[10:13]][1] + registers[inst[13:]][1]
         if registers[inst[7:10]][1] > 255:
             registers[inst[7:10]][1] = 0
             registers["111"][0] = 1
 
-    elif OPcode[instruction[0:5]][0] == "sub":
+    elif OPcode[inst[0:5]][0] == "sub":
         registers[inst[7:10]][1] = registers[inst[10:13]][1]-registers[inst[13:]][1]
         if registers[inst[7:10]][1] < 0:
             registers[inst[7:10]][1] = 0
             registers["111"][0] = 1
 
-    elif OPcode[instruction[0:5]][0] == "mul":
+    elif OPcode[inst[0:5]][0] == "mul":
         registers[inst[7:10]][1] = registers[inst[10:13]][1] * registers[inst[13:]][1]
         if registers[inst[7:10]][1] > 255:
             registers[inst[7:10]][1] = 0
             registers["111"][0] = 1
 
-    elif OPcode[instruction[0:5]][0] == "or":
+    elif OPcode[inst[0:5]][0] == "or":
         registers[inst[7:10]][1] = registers[inst[10:13]][1] | registers[inst[13:]][1]
 
 
-    elif OPcode[instruction[0:5]][0] == "and":
+    elif OPcode[inst[0:5]][0] == "and":
         registers[inst[7:10]][1] = registers[inst[10:13]][1] & registers[inst[13:]][1]
 
-    elif OPcode[instruction[0:5]][0] == "xor":
+    elif OPcode[inst[0:5]][0] == "xor":
         registers[inst[7:10]][1] = registers[inst[10:13]][1] ^ registers[inst[13:]][1]
 
-def b_type(inst):
-    if OPcode[instruction[0:5]][0] == "mov":
-        registers[instruction[5:8]][1] = binaryToDecimal(instruction[8:])
 
-    elif OPcode[instruction[0:5]][0] == "rs":
-        registers[instruction[5:8]][1] = registers[instruction[5:8]][1]>>binaryToDecimal(instruction[8:])
+def b_type(inst):
+    if OPcode[inst[0:5]][0] == "mov":
+        registers[inst[5:8]][1] = binaryToDecimal(inst[8:])
+
+    elif OPcode[inst[0:5]][0] == "rs":
+        registers[inst[5:8]][1] = registers[inst[5:8]][1]>>binaryToDecimal(inst[8:])
 
     elif OPcode[instruction[0:5]][0] == "ls":
-        registers[instruction[5:8]][1] = registers[instruction[5:8]][1]<<binaryToDecimal(instruction[8:])
+        registers[inst[5:8]][1] = registers[inst[5:8]][1]<<binaryToDecimal(inst[8:])
+
 
 def c_type(inst):
-    if OPcode[instruction[0:5]][0] == "div":
-        registers["R0"][1] = binaryToDecimal(instruction[10:13])/binaryToDecimal(instruction[13:])
-        registers["R1"][1] = binaryToDecimal(instruction[10:13])%binaryToDecimal(instruction[13:])
 
+    if OPcode[inst[0:5]][0] == "div":
+        registers["R0"][1] = binaryToDecimal(inst[10:13])/binaryToDecimal(inst[13:])
+        registers["R1"][1] = binaryToDecimal(inst[10:13])%binaryToDecimal(inst[13:])
 
-    elif OPcode[instruction[0:5]][0] == "not":
-        registers[instruction[10:13]][1] = ~binaryToDecimal(instruction[13:])
+    elif OPcode[inst[0:5]][0] == "not":
+        registers[inst[10:13]][1] = ~binaryToDecimal(inst[13:])
 
-    elif OPcode[instruction[0:5]][0] == "cmp":
-        registers[instruction[10:13]][1] = ~binaryToDecimal(instruction[13:])
-        if registers[instruction[10:13]][1] > registers[instruction[13:]][1]:
+    elif OPcode[inst[0:5]][0] == "cmp":
+        registers[inst[10:13]][1] = ~binaryToDecimal(inst[13:])
+        if registers[inst[10:13]][1] > registers[inst[13:]][1]:
             registers["FLAGS"][1][2]=1
-        elif registers[instruction[10:13]][1] < registers[instruction[13:]][1]:
+        elif registers[inst[10:13]][1] < registers[inst[13:]][1]:
             registers["FLAGS"][1][2] = 1
-        elif registers[instruction[10:13]][1] == registers[instruction[13:]][1]:
+        elif registers[inst[10:13]][1] == registers[inst[13:]][1]:
             registers["FLAGS"][1][3] = 1
-    elif OPcode[instruction[0:5]][0] == "mov":
-        registers[instruction[10:13]][1] = registers[instruction[13:]][1]
+    elif OPcode[inst[0:5]][0] == "mov":
+        registers[inst[10:13]][1] = registers[inst[13:]][1]
+
+
+def d_type(inst):
+    if OPcode[inst[0:5]][0] == "ld":
+        registers[inst[5:8]][1] = 
+
+
+
 
 def execute(inst):
-    if OPcode[instruction[0:5]][1] == "A":
+    if OPcode[inst[0:5]][1] == "A":
         a_type(inst)
-    elif OPcode[instruction[0:5]][1] == "B":
+    elif OPcode[inst[0:5]][1] == "B":
         b_type(inst)
-    elif OPcode[instruction[0:5]][1] == "C":
+    elif OPcode[inst[0:5]][1] == "C":
          c_type(inst)
-    # elif OPcode[instruction[0:5]][1] == "D":
+    # elif OPcode[inst[0:5]][1] == "D":
     #     #d_type(inst)
-    # elif OPcode[instruction[0:5]][1] == "E":
+    # elif OPcode[inst[0:5]][1] == "E":
     #     #e_type(inst)
 
 
@@ -125,5 +137,27 @@ while mem[PC][0:5] != "10011":
 
 
 print(registers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
