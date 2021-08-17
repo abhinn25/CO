f3bf5f08e68041667d110a47e47db024362744ca
abhinn25 <88654431+abhinn25@users.noreply.github.com>
2021-08-18 04:09:05 +0530

    Update simulator.py

--- CO_M21_Assignment-main/SimpleSimulator/simulator.py
+++ CO_M21_Assignment-main/SimpleSimulator/simulator.py
@@ -1,6 +1,6 @@
 f = open("input.txt", "r")
 lines = [line.rstrip() for line in f]
-#print(lines)
+# print(lines)
 OPcode = {"00000": ("add", "A"),
           "00001": ("sub", "A"),
           "00110": ("mul", "A"),
@@ -32,22 +32,21 @@ registers = {"000": ["R0", 0],
              "111": ["FLAGS", [0, 0, 0, 0]]  # V / L / G / E
              }
 
-PC = 0
-mem = ["0000000000000000"]*256
+mem = ["0000000000000000"] * 256
 
 for i in range(len(lines)):
-        mem[i] = lines[i]
+    mem[i] = lines[i]
 
-#print(registers)
-#print(mem , len(mem))
+
+# print(registers)
+# print(mem , len(mem))
 
 
 def binaryToDecimal(n):
-    return int(n,2)
+    return int(n, 2)
 
 
 def a_type(inst):
-
     if OPcode[inst[0:5]][0] == "add":
         registers[inst[7:10]][1] = registers[inst[10:13]][1] + registers[inst[13:]][1]
         if registers[inst[7:10]][1] > 255:
@@ -55,7 +54,7 @@ def a_type(inst):
             registers["111"][0] = 1
 
     elif OPcode[inst[0:5]][0] == "sub":
-        registers[inst[7:10]][1] = registers[inst[10:13]][1]-registers[inst[13:]][1]
+        registers[inst[7:10]][1] = registers[inst[10:13]][1] - registers[inst[13:]][1]
         if registers[inst[7:10]][1] < 0:
             registers[inst[7:10]][1] = 0
             registers["111"][0] = 1
@@ -78,42 +77,75 @@ def a_type(inst):
 
 
 def b_type(inst):
+
+    flagreset()
+
     if OPcode[inst[0:5]][0] == "mov":
         registers[inst[5:8]][1] = binaryToDecimal(inst[8:])
 
     elif OPcode[inst[0:5]][0] == "rs":
-        registers[inst[5:8]][1] = registers[inst[5:8]][1]>>binaryToDecimal(inst[8:])
+        registers[inst[5:8]][1] = registers[inst[5:8]][1] >> binaryToDecimal(inst[8:])
 
     elif OPcode[instruction[0:5]][0] == "ls":
-        registers[inst[5:8]][1] = registers[inst[5:8]][1]<<binaryToDecimal(inst[8:])
+        registers[inst[5:8]][1] = registers[inst[5:8]][1] << binaryToDecimal(inst[8:])
 
 
 def c_type(inst):
 
     if OPcode[inst[0:5]][0] == "div":
-        registers["R0"][1] = binaryToDecimal(inst[10:13])/binaryToDecimal(inst[13:])
-        registers["R1"][1] = binaryToDecimal(inst[10:13])%binaryToDecimal(inst[13:])
+        flagreset()
+        registers["000"][1] = binaryToDecimal(inst[10:13]) / binaryToDecimal(inst[13:])
+        registers["001"][1] = binaryToDecimal(inst[10:13]) % binaryToDecimal(inst[13:])
 
     elif OPcode[inst[0:5]][0] == "not":
+        flagreset()
         registers[inst[10:13]][1] = ~binaryToDecimal(inst[13:])
 
     elif OPcode[inst[0:5]][0] == "cmp":
-        registers[inst[10:13]][1] = ~binaryToDecimal(inst[13:])
+
         if registers[inst[10:13]][1] > registers[inst[13:]][1]:
-            registers["FLAGS"][1][2]=1
+            registers["111"][1][2] = 1
         elif registers[inst[10:13]][1] < registers[inst[13:]][1]:
-            registers["FLAGS"][1][2] = 1
+            registers["111"][1][1] = 1
         elif registers[inst[10:13]][1] == registers[inst[13:]][1]:
-            registers["FLAGS"][1][3] = 1
+            registers["111"][1][3] = 1
+
     elif OPcode[inst[0:5]][0] == "mov":
-        registers[inst[10:13]][1] = registers[inst[13:]][1]
+
+        if inst[13:] == "111":
+            registers[inst[10:13]][1] = 8*registers["111"][1][0] + 4*registers["111"][1][1] + 2*registers["111"][1][2] + registers["111"][1][3]
+        else:
+            flagreset()
+            registers[inst[10:13]][1] = registers[inst[13:]][1]
+
+
+var_dic = {}
 
 
 def d_type(inst):
-    if OPcode[inst[0:5]][0] == "ld":
-        registers[inst[5:8]][1] = 
+    flagreset()
+    if OPcode[inst[0:5]][0] == "st":
+        var_dic[inst[8:]] = registers[inst[5:8]][1]
+
+    elif OPcode[inst[0:5]][0] == "ld":
+        registers[inst[5:8]][1] = var_dic[inst[8:]]
 
 
+def e_type(inst):
+    global PC
+    if OPcode[inst[0:5]][0] == "jmp":
+        PC = binaryToDecimal(inst[8:])-1
+
+    elif OPcode[inst[0:5]][0] == "jlt" and registers["111"][1][1] == 1:
+        PC = binaryToDecimal(inst[8:])-1
+
+    elif OPcode[inst[0:5]][0] == "jgt" and registers["111"][1][2] == 1:
+        PC = binaryToDecimal(inst[8:])-1
+
+    elif OPcode[inst[0:5]][0] == "je" and registers["111"][1][3] == 1:
+        PC = binaryToDecimal(inst[8:])-1
+
+    flagreset()
 
 
 def execute(inst):
@@ -122,42 +154,35 @@ def execute(inst):
     elif OPcode[inst[0:5]][1] == "B":
         b_type(inst)
     elif OPcode[inst[0:5]][1] == "C":
-         c_type(inst)
-    # elif OPcode[inst[0:5]][1] == "D":
-    #     #d_type(inst)
-    # elif OPcode[inst[0:5]][1] == "E":
-    #     #e_type(inst)
+        c_type(inst)
+    elif OPcode[inst[0:5]][1] == "D":
+        d_type(inst)
+    elif OPcode[inst[0:5]][1] == "E":
+        e_type(inst)
+
+
+PC = 0
+
+
+def printreg():
+    print(dectobin(registers["000"][1]), dectobin(registers["001"][1]), dectobin(registers["010"][1]), dectobin(registers["011"][1]), dectobin(registers["100"][1]), dectobin(registers["101"][1]), dectobin(registers["110"][1]), dectobin( 8*registers["111"][1][0] + 4*registers["111"][1][1] + 2*registers["111"][1][2] + registers["111"][1][3]))
+
+def dectobin(value):
+    return "0" * (16 - len(bin(value)[2:])) + bin(value)[2:]
+
+
+def flagreset():
+    registers["111"][1] = [0, 0, 0, 0]
 
 
 while mem[PC][0:5] != "10011":
-
+    #print(PC)
     instruction = mem[PC]
     execute(instruction)
+    print("0" * (8 - len(bin(PC)[2:])) + bin(PC)[2:], end= " ")
+    printreg()
     PC += 1
 
-
-print(registers)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in mem:
+    print(i)
+#print(registers)
